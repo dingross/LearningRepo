82f8df25fc2148466bd8b662b1e64574c7b3fb63
dingross <din@interai.co>
2022-06-14 17:01:23 +0300

    another commit, with a change in the message

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('I am a noob in Pokemon')
+    print_hi('I am a noob in Pokemon, but I will get better')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
